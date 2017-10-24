6d21c22b84a69bf753f9fde5e0d69c66a2126c63
hilarybrown <hilary.brown@libertymutual.com>
2017-10-24 12:00:34 -0400

    just testing out destroy with a resource

--- scripts/movies/destroy.sh
+++ scripts/movies/destroy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --include --request DELETE "http://localhost:4741/movies/${ID}" --header "Authorization: Token token=$TOKEN"
+curl --include --request DELETE "http://localhost:4741/movies/${ID}" --header "Authorization: Token token=BAhJIiVlNzZmNjc4YTM0MmUyNDQ1Nzg4NThjNzNjYmI3MjBjNQY6BkVG--ae7b6c8e3dcbe543f1a32fe375a9dcc3251ea631"
