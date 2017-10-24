f3615ae9ea14d5f2557db36db69eccac7657ebf0
hilarybrown <hilary.brown@libertymutual.com>
2017-10-24 12:16:52 -0400

    add current_user to get-index

--- scripts/movies/get-index.sh
+++ scripts/movies/get-index.sh
@@ -5,6 +5,6 @@ URL_PATH="/movies"
 curl "${API}${URL_PATH}" \
   --include \
   --request GET \
-  --header "Authorization: Token token=$TOKEN"
+  --header "Authorization: Token token=BAhJIiViZDQ5YjViOWVhMWVjNTUzM2Q3NDFlMjVmZGE5NmIzMgY6BkVG--51c2ed8d8f1115add79facffdbd49584f71b46ba"
 
 echo
